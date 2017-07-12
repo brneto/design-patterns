6197b17c6f81d1609a6365cbd2a8cbbf196755bf
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-07-12 14:52:51 -0300

    Implement de classe that uses the Decorator pattern

--- decorator/src/main/java/starbuzzcoffe/StarbuzzCoffeeOrder.java
+++ decorator/src/main/java/starbuzzcoffe/StarbuzzCoffeeOrder.java
@@ -1,9 +1,33 @@
 package starbuzzcoffe;
 
-public class StarbuzzCoffeeOrder {
+import java.util.Locale;
 
+import starbuzzcoffe.component.Beverage;
+import starbuzzcoffe.component.DarkRoast;
+import starbuzzcoffe.component.Espresso;
+import starbuzzcoffe.component.HouseBlend;
+import starbuzzcoffe.decorator.Mocha;
+import starbuzzcoffe.decorator.Whip;
+
+public class StarbuzzCoffeeOrder {
+	
 	public static void main (String[] args) {
+		Beverage order;
 		
+		order = new Espresso();
+		System.out.println(order.getDescription() + " $" + String.format(Locale.US, "%.2f", order.cost()));
+		
+		order = new DarkRoast();
+		order = new Mocha(order);
+		order = new Mocha(order);
+		order = new Whip(order);
+		System.out.println(order.getDescription() + " $" + String.format(Locale.US, "%.2f", order.cost()));
+		
+		order = new HouseBlend();
+		order = new Mocha(order);
+		order = new Mocha(order);
+		order = new Whip(order);
+		System.out.println(order.getDescription() + " $" + String.format(Locale.US, "%.2f", order.cost()));
 	}
 
 }

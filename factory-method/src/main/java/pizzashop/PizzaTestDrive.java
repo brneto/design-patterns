56c39dd26b1e010af36078ccd1474a5fe8cb067b
Berchris Requiao <brneto@icloud.com>
2017-07-16 17:07:41 -0300

    Update the project name according with the pattern name its implement

--- factory-method/src/main/java/pizzashop/PizzaTestDrive.java
+++ factory-method/src/main/java/pizzashop/PizzaTestDrive.java
@@ -1,5 +1,6 @@
 package pizzashop;
 
+import pizzashop.factory.CaliforniaPizzaStore;
 import pizzashop.factory.ChicagoPizzaStore;
 import pizzashop.factory.NYPizzaStore;
 import pizzashop.factory.PizzaStore;
@@ -11,12 +12,16 @@ public class PizzaTestDrive {
 	public static void main(String[] args) {
 		PizzaStore nyStore = new NYPizzaStore();
 		PizzaStore chicagoStore = new ChicagoPizzaStore();
+		PizzaStore californiaStore = new CaliforniaPizzaStore();
 		
 		Pizza pizza = nyStore.orderPizza(PizzaType.CHEESE);
 		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
 		
 		pizza = chicagoStore.orderPizza(PizzaType.CHEESE);
 		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+		
+		pizza = californiaStore.orderPizza(PizzaType.CHEESE);
+		System.out.println("Someone ordered a " + pizza.getName() + "\n");
 		
 	}
 

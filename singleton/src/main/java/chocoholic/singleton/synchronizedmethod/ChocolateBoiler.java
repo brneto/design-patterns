59f344d06cea15fd03de6cdc27b3c37266138175
Berchris Requiao <brneto@icloud.com>
2017-07-19 23:21:20 -0300

    Add 3 kinds of Singleton implementation
    The approaches used have been:
    1. Synchronized Method;
    2. Eager instantiation;
    3. Double-checked locking.

--- singleton/src/main/java/chocoholic/singleton/synchronizedmethod/ChocolateBoiler.java
+++ singleton/src/main/java/chocoholic/singleton/synchronizedmethod/ChocolateBoiler.java
@@ -1,7 +1,7 @@
-package chocoholic.singleton;
+package chocoholic.singleton.synchronizedmethod;
 
 public class ChocolateBoiler {
-	private volatile static ChocolateBoiler instance;
+	private static ChocolateBoiler instance;
 	private boolean empty;
 	private boolean boiled;
 
@@ -9,16 +9,14 @@ public class ChocolateBoiler {
 		empty = true;
 		boiled = false;
 	}
-	
-	public static ChocolateBoiler getInstance() {
-		if (instance == null)
-			synchronized (ChocolateBoiler.class) {
-				if (instance == null)
-					instance = new ChocolateBoiler();
-			}
+
+	public synchronized static ChocolateBoiler getInstance() {
+		if (instance == null) {
+			instance = new ChocolateBoiler();
+		}
 		return instance;
 	}
-	
+
 	public void fill() {
 		if (isEmpty()) {
 			empty = false;
@@ -26,25 +24,25 @@ public class ChocolateBoiler {
 			// fill the boiler with a milk/chocolate mixture
 		}
 	}
-	
+
 	public void drain() {
 		if (!isEmpty() && isBoiled()) {
 			// drain the boiled milk and chocolate
 			empty = true;
 		}
 	}
-	
+
 	public void boil() {
 		if (!isEmpty() && !isBoiled()) {
 			// bring the contents to a boil
 			boiled = true;
 		}
 	}
-	
+
 	public boolean isEmpty() {
 		return empty;
 	}
-	
+
 	public boolean isBoiled() {
 		return boiled;
 	}

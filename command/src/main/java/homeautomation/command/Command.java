21238d9d4d0da9ca4eba3b48225fa882a06a5cc9
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-07-21 19:08:07 -0300

    Add first command and receiver classes

---
+++ command/src/main/java/homeautomation/command/Command.java
@@ -0,0 +1,5 @@
+package homeautomation.command;
+
+public interface Command {
+	public void execute();
+}
---
+++ command/src/main/java/homeautomation/receiver/Light.java
@@ -0,0 +1,13 @@
+package homeautomation.receiver;
+
+public class Light {
+
+	public void on() {
+		System.out.println("Lights on!");
+	}
+
+	public void off() {
+		System.out.println("Lights off!");
+	}
+
+}

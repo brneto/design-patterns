311e57322439b0df2e976932b847096fd041fbd7
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-07-21 20:57:34 -0300

    Add new receiver class GarageDoor

---
+++ command/src/main/java/homeautomation/receiver/GarageDoor.java
@@ -0,0 +1,25 @@
+package homeautomation.receiver;
+
+public class GarageDoor {
+
+	public void up() {
+		System.out.println("Garage door is Up");
+	}
+
+	public void down() {
+		System.out.println("Garage door is Down");
+	}
+
+	public void stop() {
+		System.out.println("Garage door has stopped");
+	}
+
+	public void lightOn() {
+		System.out.println("Garage light is On");
+	}
+
+	public void lightOff() {
+		System.out.println("Garage light is Off");
+	}
+
+}

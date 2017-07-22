438fde9957ff708c547539f292f728bf27e80fbd
Berchris Requiao <brneto@icloud.com>
2017-07-21 23:48:56 -0300

    Add new command class and update the requester of it

---
+++ command/src/main/java/homeautomation/command/GarageDoorOpenCommand.java
@@ -0,0 +1,18 @@
+package homeautomation.command;
+
+import homeautomation.receiver.GarageDoor;
+
+public class GarageDoorOpenCommand implements Command {
+	GarageDoor garageDoor;
+
+	public GarageDoorOpenCommand(GarageDoor garageDoor) {
+		this.garageDoor = garageDoor;
+	}
+
+	@Override
+	public void execute() {
+		garageDoor.lightOn();
+		garageDoor.up();
+	}
+
+}
--- command/src/main/java/homeautomation/requester/RemoteControlTest.java
+++ command/src/main/java/homeautomation/requester/RemoteControlTest.java
@@ -1,16 +1,22 @@
 package homeautomation.requester;
 
 import homeautomation.command.Command;
+import homeautomation.command.GarageDoorOpenCommand;
 import homeautomation.command.LightOnCommand;
 import homeautomation.invoker.RemoteControl;
+import homeautomation.receiver.GarageDoor;
 import homeautomation.receiver.Light;
 
 public class RemoteControlTest {
 	public static void main(String[] args) {
 		RemoteControl remote = new RemoteControl();
 		Command lightOn = new LightOnCommand(new Light());
+		Command garageOpen = new GarageDoorOpenCommand(new GarageDoor());
 		
 		remote.setCommand(lightOn);
+		remote.buttonWasPressed();
+		
+		remote.setCommand(garageOpen);
 		remote.buttonWasPressed();
 	}
 

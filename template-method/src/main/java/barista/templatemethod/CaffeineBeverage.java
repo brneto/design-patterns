1844d1bc92082a58587f3bf0c543b984f40b2796
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-07-26 18:55:57 -0300

    Add and use Hook method

--- template-method/src/main/java/barista/templatemethod/CaffeineBeverage.java
+++ template-method/src/main/java/barista/templatemethod/CaffeineBeverage.java
@@ -1,24 +1,31 @@
 package barista.templatemethod;
 
 public abstract class CaffeineBeverage {
-	
+
 	// This method is a Template Method
 	public final void prepareRecipe() {
 		boilWater();
 		brew();
 		pourInCup();
-		addCondiments();
+		if (customerWantsCondiments()) {
+			addCondiments();
+		}
 	}
 
 	public abstract void brew();
 
 	public abstract void addCondiments();
 
-	public void boilWater() {
+	public final void boilWater() {
 		System.out.println("Boiling water");
 	}
 
-	public void pourInCup() {
+	public final void pourInCup() {
 		System.out.println("Pouring into cup");
+	}
+	
+	// This method is a Hook on Template Method
+	public boolean customerWantsCondiments() {
+		return true;
 	}
 }

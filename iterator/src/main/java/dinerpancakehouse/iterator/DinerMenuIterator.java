442c8e413b948246c66378b65b1e611280b28a24
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-08-03 15:59:56 -0300

    Add Interface method remove in Iterator

--- iterator/src/main/java/dinerpancakehouse/iterator/DinerMenuIterator.java
+++ iterator/src/main/java/dinerpancakehouse/iterator/DinerMenuIterator.java
@@ -20,12 +20,26 @@ public class DinerMenuIterator implements Iterator<MenuItem> {
 		else
 			throw new NoSuchElementException();
 	}
-	
+
 	@Override
 	public boolean hasNext() {
-		if(position >= items.length || items[position] == null)
+		if (position >= items.length || items[position] == null)
 			return false;
 		return true;
+	}
+
+	@Override
+	public void remove() {
+		if (position <= 0) {
+			throw new IllegalStateException
+				("You can’t remove an item until you’ve done at least one next()");
+		}
+		if (items[position - 1] != null) {
+			for (int i = position - 1; i < (items.length - 1); i++) {
+				items[i] = items[i + 1];
+			}
+			items[items.length - 1] = null;
+		}
 	}
 
 }

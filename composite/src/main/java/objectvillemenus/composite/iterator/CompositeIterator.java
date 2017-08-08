bf4a29352d4139579bb3536ff152f30d7529bcd9
Berchris Requiao <berchrisrequiao@mpf.mp.br>
2017-08-08 17:04:13 -0300

    Fix the CompositeIterator class

--- composite/src/main/java/objectvillemenus/composite/iterator/CompositeIterator.java
+++ composite/src/main/java/objectvillemenus/composite/iterator/CompositeIterator.java
@@ -5,22 +5,20 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 import objectvillemenus.component.MenuComponent;
-import objectvillemenus.composite.Menu;
 
 public class CompositeIterator implements Iterator<MenuComponent> {
-	Stack<Iterable<MenuComponent>> stack = new Stack<Iterable<MenuComponent>>();
+	Stack<Iterator<MenuComponent>> stack = new Stack<Iterator<MenuComponent>>();
 
 	public CompositeIterator(Iterable<MenuComponent> iterable) {
-		stack.push(iterable);
+		stack.push(iterable.iterator());
 	}
 
 	@Override
 	public MenuComponent next() {
 		if (hasNext()) {
-			MenuComponent component = stack.peek().iterator().next();
-			if (component instanceof Menu) {
-				stack.push(component);
-			}
+			Iterator<MenuComponent> iterator = stack.peek();
+			MenuComponent component = iterator.next();
+			stack.push(component.iterator());
 			return component;
 		} else {
 			throw new NoSuchElementException();
@@ -32,7 +30,8 @@ public class CompositeIterator implements Iterator<MenuComponent> {
 		if (stack.empty()) {
 			return false;
 		} else {
-			if (!stack.peek().iterator().hasNext()) {
+			Iterator<MenuComponent> iterator = stack.peek();
+			if (!iterator.hasNext()) {
 				stack.pop();
 				return hasNext();
 			} else {

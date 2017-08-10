538cbe8ecb9043bfa32f3d62779199b25afe5a5d
Berchris Requiao <brneto@icloud.com>
2017-08-09 22:00:41 -0300

    Add new methods to GumballMachine class

--- state/src/main/java/mightygumball/GumballMachine.java
+++ state/src/main/java/mightygumball/GumballMachine.java
@@ -5,21 +5,42 @@ public class GumballMachine {
 	final static int NO_QUARTER = 1;
 	final static int HAS_QUARTER = 2;
 	final static int SOLD = 3;
-	
+
 	int state = SOLD_OUT;
+	int count = 0;
+	
+	public GumballMachine(int count) {
+		this.count = count;
+		if (count > 0)
+			state = NO_QUARTER;
+	}
 
 	public void insertQuarter() {
 		switch (state) {
 		case HAS_QUARTER:
-			print("You can't insert another quarter");
+			System.out.println("You can't insert another quarter");
+			break;
+		case NO_QUARTER:
+			state = HAS_QUARTER;
+			System.out.println("You inserted a quarter");
+			break;
+		case SOLD_OUT:
+			System.out.println("You can’t insert a quarter, the machine is sold out");
+			break;
+		case SOLD:
+			System.out.println("Please wait, we’re already giving you a gumball");
+			break;
 		}
-		
 	}
 	
+	public void ejectQuarter() {
+		switch (state) {
+		
+		}
+	}
+
 	private void print(String value) {
 		System.out.println(value);
 	}
-	
-	public void main()
 
 }

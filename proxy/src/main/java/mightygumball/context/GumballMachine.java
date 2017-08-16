bc5b27b927c9206680aa6a255d26155491a23190
Berchris Requiao <brneto@icloud.com>
2017-08-15 22:48:31 -0300

    Alter Context class to support Monitor

---
+++ proxy/src/main/java/mightygumball/context/GumballMachine.java
@@ -0,0 +1,101 @@
+package mightygumball.context;
+
+import mightygumball.state.State;
+import mightygumball.state.concrete.HasQuarterState;
+import mightygumball.state.concrete.NoQuarterState;
+import mightygumball.state.concrete.SoldOutState;
+import mightygumball.state.concrete.SoldState;
+import mightygumball.state.concrete.WinnerState;
+
+public class GumballMachine {
+	State soldOutState, noQuarterState, hasQuarterState, soldState, winnerState;
+	State state = soldOutState;
+	int count = 0;
+	String location;
+	
+	public GumballMachine(String location, int numberGumballs) {
+		soldOutState = new SoldOutState(this);
+		noQuarterState = new NoQuarterState(this);
+		hasQuarterState = new HasQuarterState(this);
+		soldState = new SoldState(this);
+		winnerState = new WinnerState(this);
+	
+		this.count = numberGumballs;
+		if (numberGumballs > 0) {
+			state = noQuarterState;
+		}
+		
+		this.location = location;
+	}
+
+	public void insertQuarter() {
+		state.insertQuarter();
+	}
+	
+	public void ejectQuarter() {
+		state.ejectQuarter();
+	}
+	
+	public void turnCrank() {
+		state.turnCrank();
+		state.dispense();
+	}
+	
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	public void releaseBall() {
+		System.out.println("A gumball comes rolling out the slot...");
+		if (count != 0) {
+			count = count - 1;
+		}
+	}
+	
+	public State getHasQuarterState() {
+		return hasQuarterState;
+	}
+	
+	public State getNoQuarterState() {
+		return noQuarterState;
+	}
+	
+	public State getSoldState() {
+		return soldState;
+	}
+	
+	public State getSoldOutState() {
+		return soldOutState;
+	}
+	
+	public State getWinnerState() {
+		return winnerState;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public void refill(int count) {
+		this.count += count;
+		System.out.println("The gumball machine was just refilled; it's new count is: " + this.count);
+		state.refill();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		result.append("\nMighty Gumball, Inc.");
+		result.append("\nJava-enabled Standing Gumball Model #2004\n");
+		result.append("Inventory: " + count + " gumball");
+		
+		if (count != 1) {
+			result.append("s");
+		}
+		
+		result.append("\nMachine is " + state + "\n");
+		
+		return result.toString();
+	}
+
+}
---
+++ proxy/src/main/java/mightygumball/state/concrete/HasQuarterState.java
@@ -0,0 +1,54 @@
+package mightygumball.state.concrete;
+
+import java.util.Random;
+
+import mightygumball.context.GumballMachine;
+import mightygumball.state.State;
+
+public class HasQuarterState implements State {
+	Random randomWinner = new Random(System.currentTimeMillis());
+	GumballMachine gumballMachine;
+
+	public HasQuarterState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	@Override
+	public void insertQuarter() {
+		System.out.println("You can’t insert another quarter");
+	}
+
+	@Override
+	public void ejectQuarter() {
+		System.out.println("Quarter returned");
+		gumballMachine.setState(gumballMachine.getNoQuarterState());
+	}
+
+	@Override
+	public void turnCrank() {
+		System.out.println("You turned...");
+		int winner = randomWinner.nextInt(10);
+		if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+			gumballMachine.setState(gumballMachine.getWinnerState());
+		} else {
+			gumballMachine.setState(gumballMachine.getSoldState());
+		}
+
+	}
+
+	@Override
+	public void dispense() {
+		System.out.println("No gumball dispensed");
+	}
+	
+	@Override
+	public void refill() {
+
+	}
+	
+	@Override
+	public String toString() {
+		return "waiting for turn of crank";
+	}
+
+}

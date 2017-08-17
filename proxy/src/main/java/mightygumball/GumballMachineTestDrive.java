266e77b848d4467f91ac0b8f6a93e3aacdc09646
Berchris Requiao <brneto@icloud.com>
2017-08-16 22:38:26 -0300

    Implement TestDrive class

--- proxy/src/main/java/mightygumball/GumballMachineTestDrive.java
+++ proxy/src/main/java/mightygumball/GumballMachineTestDrive.java
@@ -1,42 +1,27 @@
 package mightygumball;
 
 import mightygumball.context.GumballMachine;
+import mightygumball.monitor.GumballMonitor;
 
 public class GumballMachineTestDrive {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter();
-		gumballMachine.ejectQuarter();
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-		gumballMachine.ejectQuarter();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter();
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+		String[] option = {"Seattle", "112"};
+		args = option;
+		
+		int count = 0;
+		
+		if (args.length < 2) {
+			System.out.println("GumballMachine <name> <inventory>");
+			System.exit(1);
+		}
+		
+		count = Integer.parseInt(args[1]);
+		GumballMachine gumballMachine = new GumballMachine(args[0], count);
+		
+		GumballMonitor monitor = new GumballMonitor(gumballMachine);
+		
+		monitor.report();
 	}
 
 }
--- proxy/src/main/java/mightygumball/context/GumballMachine.java
+++ proxy/src/main/java/mightygumball/context/GumballMachine.java
@@ -12,22 +12,22 @@ public class GumballMachine {
 	State state = soldOutState;
 	int count = 0;
 	String location;
-	
+
 	public GumballMachine(String location, int numberGumballs) {
 		soldOutState = new SoldOutState(this);
 		noQuarterState = new NoQuarterState(this);
 		hasQuarterState = new HasQuarterState(this);
 		soldState = new SoldState(this);
 		winnerState = new WinnerState(this);
-	
+
 		this.count = numberGumballs;
 		if (numberGumballs > 0) {
 			state = noQuarterState;
 		}
-		
+
 		this.location = location;
 	}
-	
+
 	public String getLocation() {
 		return location;
 	}
@@ -35,75 +35,74 @@ public class GumballMachine {
 	public void insertQuarter() {
 		state.insertQuarter();
 	}
-	
+
 	public void ejectQuarter() {
 		state.ejectQuarter();
 	}
-	
+
 	public void turnCrank() {
 		state.turnCrank();
 		state.dispense();
 	}
-	
+
 	public void setState(State state) {
 		this.state = state;
 	}
-	
+
 	public void releaseBall() {
 		System.out.println("A gumball comes rolling out the slot...");
 		if (count != 0) {
 			count = count - 1;
 		}
 	}
-	
+
 	public State getHasQuarterState() {
 		return hasQuarterState;
 	}
-	
+
 	public State getNoQuarterState() {
 		return noQuarterState;
 	}
-	
+
 	public State getSoldState() {
 		return soldState;
 	}
-	
+
 	public State getSoldOutState() {
 		return soldOutState;
 	}
-	
+
 	public State getWinnerState() {
 		return winnerState;
 	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+
 	public void refill(int count) {
 		this.count += count;
 		System.out.println("The gumball machine was just refilled; it's new count is: " + this.count);
 		state.refill();
 	}
-	
-	@Override
-	public String toString() {
-		StringBuffer result = new StringBuffer();
-		result.append("\nMighty Gumball, Inc.");
-		result.append("\nJava-enabled Standing Gumball Model #2004\n");
-		result.append("Inventory: " + count + " gumball");
-		
-		if (count != 1) {
-			result.append("s");
-		}
-		
-		result.append("\nMachine is " + state + "\n");
-		
-		return result.toString();
+
+	public int getCount() {
+		return count;
 	}
 
 	public State getState() {
 		return state;
 	}
 
+	@Override
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		result.append("\nMighty Gumball, Inc.");
+		result.append("\nJava-enabled Standing Gumball Model #2004\n");
+		result.append("Inventory: " + count + " gumball");
+
+		if (count != 1) {
+			result.append("s");
+		}
+
+		result.append("\nMachine is " + state + "\n");
+
+		return result.toString();
+	}
 }

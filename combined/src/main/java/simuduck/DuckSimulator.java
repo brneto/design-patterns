d1bb8eb17b7fb7422eaf90c62178a90b83a2d54c
Berchris Requiao <brneto@icloud.com>
2017-08-20 13:01:44 -0300

    Add a class for simulator

---
+++ combined/src/main/java/simuduck/DuckSimulator.java
@@ -0,0 +1,21 @@
+package simuduck;
+
+import simuduck.behavior.Quackable;
+
+public class DuckSimulator {
+
+	public static void main(String[] args) {
+		DuckSimulator simulator = new DuckSimulator();
+		simulator.simulate();
+	}
+
+	void simulate() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	void simulate(Quackable duck) {
+		
+	}
+
+}

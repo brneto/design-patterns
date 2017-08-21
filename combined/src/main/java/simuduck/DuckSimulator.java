9be9db83ea33300afc0e6d265e5cb804484362bc
Berchris Requiao <brneto@icloud.com>
2017-08-20 22:44:24 -0300

    Add Decorator pattern

--- combined/src/main/java/simuduck/DuckSimulator.java
+++ combined/src/main/java/simuduck/DuckSimulator.java
@@ -7,6 +7,7 @@ import simuduck.client.Goose;
 import simuduck.client.MallardDuck;
 import simuduck.client.RedheadDuck;
 import simuduck.client.RubberDuck;
+import simuduck.decorator.QuackCounter;
 
 public class DuckSimulator {
 
@@ -16,19 +17,21 @@ public class DuckSimulator {
 	}
 
 	void simulate() {
-		Quackable mallardDuck = new MallardDuck();
-		Quackable redheadDuck = new RedheadDuck();
-		Quackable duckCall = new DuckCall();
-		Quackable rubberDuck = new RubberDuck();
-		Quackable gooseDuck = new GooseAdapter(new Goose());
+		Quackable mallardDuck = new QuackCounter(new MallardDuck());
+		Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+		Quackable duckCall = new QuackCounter(new DuckCall());
+		Quackable rubberDuck = new QuackCounter(new RubberDuck());
+		Quackable gooseDuck = new QuackCounter(new GooseAdapter(new Goose()));
 
-		System.out.println("\nDuck Simulator: With goose Adapter");
+		System.out.println("\nDuck Simulator: With Adapter and Decorator");
 
 		simulate(mallardDuck);
 		simulate(redheadDuck);
 		simulate(duckCall);
 		simulate(rubberDuck);
 		simulate(gooseDuck);
+		
+		System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times");
 	}
 
 	void simulate(Quackable duck) {
---
+++ combined/src/main/java/simuduck/decorator/QuackCounter.java
@@ -0,0 +1,23 @@
+package simuduck.decorator;
+
+import simuduck.behavior.Quackable;
+
+public class QuackCounter implements Quackable {
+	private Quackable duck;
+	private static int numberOfQuacks = 0;
+	
+	public QuackCounter(Quackable duck) {
+		this.duck = duck;
+	}
+
+	@Override
+	public void quack() {
+		duck.quack();
+		numberOfQuacks++;
+	}
+	
+	public static int getQuacks() {
+		return numberOfQuacks;
+	}
+
+}

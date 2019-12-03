2c7faea663e387ea2e2a7a9a0f6dd7d334d1c66c
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-12-03 20:02:31 +0000

    Add functional style to PizzaStore

--- abstract-factory/src/main/java/pizzashop/factorymethod/PizzaStore.java
+++ abstract-factory/src/main/java/pizzashop/factorymethod/PizzaStore.java
@@ -7,13 +7,11 @@ import pizzashop.product.pizza.type.PizzaType;
 public abstract class PizzaStore {
 	
 	public final Pizza orderPizza(PizzaType type) {
-		Pizza pizza = createPizza(type);
-
-		pizza.prepare();
-		pizza.bake();
-		pizza.cut();
-		pizza.box();
-		return pizza;
+		return createPizza(type)
+			.prepare()
+			.bake()
+			.cut()
+			.box();
 	}
 
 	protected abstract Pizza createPizza(PizzaType type) throws InvalidPizzaType;

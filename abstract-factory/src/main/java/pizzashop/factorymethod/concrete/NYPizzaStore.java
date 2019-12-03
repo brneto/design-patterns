7302f50c6af2523c270c8b06479b98644c87a134
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-12-03 20:22:15 +0000

    remove duplicated code

--- abstract-factory/src/main/java/pizzashop/factorymethod/concrete/NYPizzaStore.java
+++ abstract-factory/src/main/java/pizzashop/factorymethod/concrete/NYPizzaStore.java
@@ -15,21 +15,8 @@ public class NYPizzaStore extends PizzaStore {
 	
 	@Override
 	protected Pizza createPizza(PizzaType type) throws InvalidPizzaType {
-		String style = "New York Style Cheese Pizza";
-		PizzaIngredientFactory ingredientFactory = new NYPizzaIngredientFactory();
-		
-		switch (type) {
-		case CHEESE:
-			return (new CheesePizza(ingredientFactory)).setName(style);
-		case PEPPERONI:
-			return (new PepperoniPizza(ingredientFactory)).setName(style);
-		case CLAM:
-			return (new ClamPizza(ingredientFactory)).setName(style);
-		case VEGGIE:
-			return (new VeggiePizza(ingredientFactory)).setName(style);
-		default:
-			throw new InvalidPizzaType(type);
-		}
+		return addIngredients(type, new NYPizzaIngredientFactory())
+			.setName("New York Style Cheese Pizza");
 	}
 
 }
